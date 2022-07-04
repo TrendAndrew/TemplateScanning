0db8139f3307c85c7ac6056265998b543cb73717
Andrew Longhorn <andrewlonghorn@192-168-100-225.tpgi.com.au>
2022-07-04 10:30:25 +1000

    updated templates

--- sns-topic.tf
+++ sns-topic.tf
@@ -1,7 +1,7 @@
 resource "aws_sns_topic" "cloudwatch_alarm" {
   name         = var.topic_name
   display_name = var.topic_display_name
-}
+} 
 
 resource "aws_sns_topic_subscription" "account_email" {
   endpoint  = local.this_account_email
